f0b844ba6fcc74748d6b7f28b08662f14aad1849
Ahmed <Ahmed9588406@gmail.com>
2024-04-06 00:33:34 +0200

    adding more functionalities to segments,points and graph

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -1,9 +1,18 @@
+// Class to represent the points 
+
 class Point {
     constructor(x,y){
         this.x=x;
         this.y=y;
     }
 
+    //Equals function to test if there are two points equal to each other
+    equals(point){
+        return this.x == point.x && this.y == point.y
+    }
+
+
+    // function to draw the points on canvas
     draw(ctx, size=18,color="black"){
         const rad=size/2;
         ctx.beginPath();
--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -1,9 +1,20 @@
+// Class to represent the edges between the points
 class Segment {
+    // constructor to initialize points to make line between them
     constructor(p1,p2){
         this.p1=p1
         this.p2=p2
     }
 
+    equals(seg){
+        return this.includes(seg.p1) && this.includes(seg.p2)
+    }
+
+    includes(point){
+       return this.p1.equals(point) || this.p2.equals(point) 
+    }
+
+    // This function to draw lines between points
     draw(ctx,width=2,color="black"){
         ctx.beginPath()
         ctx.lineWidth=width
